3d876ec5b5619941bb91b79098beaf0368698b82
stark042004 <stark042004@yandex.ru>
2019-11-01 16:31:40 +0300

    -added UI of quiz and menu

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-import string
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from PyQt5 import uic
+import sys
 
-print(string.punctuation)
+
+class QuestionWindow(object):
+    def setupUI(self, MainWindow):
+        MainWindow.setTitle()
+
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UIs/MenuUI.ui', self)
+        self.startButton.clicked.connect(self.start)
+        self.exitButton.clicked.connect(self.exit)
+
+    def start(self):
+        uic.loadUi('UIs/QuestionsUI.ui', self)
+
+    def exit(self):
+        sys.exit(0)
+
+
+app = QApplication(sys.argv)
+ex = Main()
+ex.show()
+sys.exit(app.exec_())
